0cc4b91a41dc81684c02975bfda6e75d1f83b112
Tawsif <challengex46@gmail.com>
2024-11-16 01:24:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/mj', async (req, res) => {
+app.get('/xl', async (req, res) => {
     const { prompt } = req.query;
     
     async function query(data) {
@@ -49,5 +49,3 @@ app.get('/mj', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
-
-Direact images response
